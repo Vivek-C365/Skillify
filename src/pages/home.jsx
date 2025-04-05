b4601befd4b97b312162d00e32e1a6c8ba45d03e
SahilAggarwal9155 <sahilaggarwal894@gmail.com>
2025-04-05 19:33:36 +0530

    implement UI for page2

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,65 +1,72 @@
-import Navbar from "../components/navbar";
-import ContentSection from "../components/ContentSection";
-import Cards from "../components/common/card";
-import { responsiveFlex } from "../styles/responsiveFlex";
+import Navbar from '../components/navbar'
+import ContentSection from '../components/ContentSection'
+import Cards from '../components/common/card'
+import { responsiveFlex } from '../styles/responsiveFlex'
+import img1 from '../assets/img1.jpg'
+import { responsiveImage } from '../components/common/responsiveImage'
+import CourseSection2 from './CourseSection2'
+
 import {
   responsiveTextH3,
   responsiveTextH6,
   responsiveTextBodyLarge,
-} from "../styles/responsiveText";
+} from '../styles/responsiveText'
+import HeadingDescriptionText from '../components/common/HeadingDescriptionText'
 
 const Home = () => {
   const sections = [
     {
       leftContent:
-        "Personalized recommendations for each student, based on their learning style and preferences.",
-      biggerText: "Simple",
+        'Personalized recommendations for each student, based on their learning style and preferences.',
+      biggerText: 'Simple',
       tags: [
-        { icon: true, text: "advantage", item: "#" },
-        { icon: true, bgColor: "transparent", item: "#" },
+        { icon: true, text: 'advantage', item: '#' },
+        { icon: true, bgColor: 'transparent', item: '#' },
         {
           icon: true,
-          item: "#",
-          bgColor: "transparent",
-          iconColor: "border border-white primary-text",
+          item: '#',
+          bgColor: 'transparent',
+          iconColor: 'border border-white primary-text',
         },
       ],
     },
     {
       reverse: true,
-      biggerText: "Powerful",
-      rightContent: <Cards />,
+      biggerText: 'Powerful',
+      rightContent: <Cards>Card Content</Cards>,
     },
     {
       leftContent:
-        "we offer the highest quality educational resources to make learning accessible",
-      biggerText: "EdTech",
+        'we offer the highest quality educational resources to make learning accessible',
+      biggerText: 'EdTech',
       tags: [
-        { text: "Get started" },
-        { icon: true, bgColor: "transparent", item: <>&#8599;</> },
+        { text: 'Get started' },
+        { icon: true, bgColor: 'transparent', item: <>&#8599;</> },
       ],
     },
     {
       reverse: true,
       leftContent: <Cards />,
-      biggerText: "Solutions",
+      biggerText: 'Solutions',
     },
-  ];
+  ]
 
   const smallAboutus = [
     {
-      text: "+80",
-      description: "Online Courses",
+      text: '+80',
+      description: 'Online Courses',
     },
     {
-      text: "10",
-      description: "Years Experience",
+      text: '10',
+      description: 'Years Experience',
     },
     {
-      text: "77",
-      description: "Top Mentors",
+      text: '77',
+      description: 'Top Mentors',
     },
-  ];
+  ]
+
+  // console.log(sections[3]);
 
   return (
     <div>
@@ -111,8 +118,36 @@ const Home = () => {
           </p>
         </div>
       </div>
-    </div>
-  );
-};
 
-export default Home;
+      <div className={`text-black p-6  md:p-8 gap-4 md:gap-8`}>
+        <div className="max-w-[1200px] flex flex-col md:gap-8 m-auto justify-center items-center ">
+          <div>
+            <p className={`${responsiveTextH3} text-center`}>
+              {' '}
+              Digital education is a key{' '}
+            </p>
+            <p className={`${responsiveTextH3} text-center`}>
+              {' '}
+              to your future success
+            </p>
+            <div className="text-center w-4 rounded-full h-1 bg-indigo-500 mx-auto mt-2 mb-2"></div>
+            <HeadingDescriptionText>On our platform you will find the largest selection pf IT courses
+              that will
+              help you develop skills and gain new knowledge in this area.</HeadingDescriptionText>
+
+            <div className={`w-[500px] h-[600px] mx-auto mb-[-100px]`}>
+              <img
+                className={`${responsiveImage}`}
+                src={img1}
+                alt="this is image 1"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+        <CourseSection2/>
+    </div>
+  )
+}
+
+export default Home
