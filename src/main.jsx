63fd4cee4a2bbdf27e0648d10699831fadfe3970
Vivek Choudhary <vivek.choudhary.0022@gmail.com>
2025-04-09 17:36:38 +0530

    Speed Insights vercel added

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { FirebaseProvider } from "./services/firebase/firebase";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import "./styles/globalStyles.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <SpeedInsights />
     <FirebaseProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </FirebaseProvider>
+    
   </StrictMode>
 );
