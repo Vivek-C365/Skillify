22ac4053cff4ee4a1b9d878bcbd187577885054e
Vivek Choudhary <95847089+Vivek-C365@users.noreply.github.com>
2025-05-02 23:08:36 +0530

    Update AuthFirebase.jsx

--- src/auth/pages/AuthFirebase.jsx
+++ src/auth/pages/AuthFirebase.jsx
@@ -8,8 +8,6 @@ import { useDispatch } from "react-redux";
 import { setUserData } from "../../features/user/pages/userProfileSlice";
 
 import Analytics from "../../assets/images/svg/13246824_5191077.svg";
-
-
 const SignUp = ({
   title,
   subtitle,
